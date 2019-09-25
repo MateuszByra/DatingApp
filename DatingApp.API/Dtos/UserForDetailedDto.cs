4c530d795a438769892e4e251710d7d026c27fde
Mateusz <mat.byra@gmail.com>
2019-09-25 19:31:50 +0200

    DatingApp.Api: Automapper configured.

--- DatingApp.API/Dtos/UserForDetailedDto.cs
+++ DatingApp.API/Dtos/UserForDetailedDto.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using DatingApp.API.Models;
 
 namespace DatingApp.API.Dtos
 {
@@ -19,6 +18,6 @@ namespace DatingApp.API.Dtos
         public string City { get; set; }
         public string Country { get; set; }
         public string PhotoUrl { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotosForDetailedDto> Photos { get; set; }
     }
 }
