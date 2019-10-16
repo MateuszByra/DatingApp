e358fffbd8523c28e3113374bca36bcef622e056
Mateusz <mat.byra@gmail.com>
2019-10-16 12:47:09 +0200

    DatingApp-SPA: Added BehaviorSubject to add any to any communiaction

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {JwtHelperService} from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
@@ -14,8 +15,14 @@ export class AuthService {
   jwtHelper = new JwtHelperService();
   decodedToken: any;
   currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
   constructor(private http: HttpClient) { }
+
+  changeMamberPhoto(photoUrl: string) {
+    this.photoUrl.next(photoUrl);
+  }
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -26,6 +33,7 @@ export class AuthService {
           localStorage.setItem('user', JSON.stringify(user.user));
           this.currentUser = user;
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          this.changeMamberPhoto(this.currentUser.photoUrl);
         }
       })
     );
