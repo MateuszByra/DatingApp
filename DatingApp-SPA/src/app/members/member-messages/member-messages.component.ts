b8eb5f485bfd52eec06a147d3ba308f42d67d87b
Mateusz <mat.byra@gmail.com>
2019-11-19 17:55:36 +0100

    SPA, API: Added the Mark as Read functionality.

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { Message } from 'src/app/_models/message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
@@ -22,8 +23,18 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
-    .subscribe(messages => this.messages = messages, error => this.alertify.error(error));
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
+      .subscribe(messages => this.messages = messages, error => this.alertify.error(error));
   }
 
   sendMessage() {
