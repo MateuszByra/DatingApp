e3c7846f14163bc78351ce920376a5b61594267c
Mateusz <mat.byra@gmail.com>
2019-11-19 18:16:48 +0100

    SPA: changed outputPath for publishing.
    API:
    wwwroot directory added to gitignore.
    Serving static files.
    FallbackController added (to find angular routes).

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -89,7 +89,14 @@ namespace DatingApp.API
             // app.UseHttpsRedirection();
             app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); /*not good for production mode*/
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new {controller = "Fallback", action = "Index"}
+                );
+            });
         }
     }
 }
