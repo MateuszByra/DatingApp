f9bf6cf43df6570a6608787bf61a223ffe61e3af
Mateusz <mat.byra@gmail.com>
2019-12-04 14:11:17 +0100

    API: Returning the users with their roles

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,24 +1,41 @@
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace DatingApp.API.Controllers
-{
+namespace DatingApp.API.Controllers {
     [ApiController]
-    [Route("api/[controller]")]
-    public class AdminController : ControllerBase
-    {
-        [Authorize(Policy = "RequireAdminRole")]
-        [HttpGet("usersWithRoles")]
-        public IActionResult GetUsersWithRoles()
-        {
-            return Ok("Only admins can see this");
+    [Route ("api/[controller]")]
+    public class AdminController : ControllerBase {
+        private readonly DataContext _context;
+        public AdminController (DataContext context) {
+            _context = context;
         }
 
-        [Authorize(Policy = "ModeratePhotoRole")]
-        [HttpGet("photosForModeration")]
-        public IActionResult GetPhotosForModeration()
-        {
-            return Ok("Admins or moderators can see this");
+        [Authorize (Policy = "RequireAdminRole")]
+        [HttpGet ("usersWithRoles")]
+        public async Task<IActionResult> GetUsersWithRoles () {
+            var userList = await _context.Users
+                .OrderBy(x => x.UserName)
+                .Select( user => new {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Roles = (from userRole in user.UserRoles
+                                join role in _context.Roles
+                                on userRole.RoleId
+                                equals role.Id
+                                select role.Name).ToList()
+                }).ToListAsync();
+
+            return Ok(userList);
+        }
+
+        [Authorize (Policy = "ModeratePhotoRole")]
+        [HttpGet ("photosForModeration")]
+        public IActionResult GetPhotosForModeration () {
+            return Ok ("Admins or moderators can see this");
         }
     }
 }
