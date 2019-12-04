a1a7cc5970165bde2f503a60efb859d13e85c5e6
Mateusz <mat.byra@gmail.com>
2019-12-04 14:27:10 +0100

    API: Added the edit roles method.

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,7 +1,10 @@
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +13,9 @@ namespace DatingApp.API.Controllers {
     [Route ("api/[controller]")]
     public class AdminController : ControllerBase {
         private readonly DataContext _context;
-        public AdminController (DataContext context) {
+        private readonly UserManager<User> _userManager;
+        public AdminController (DataContext context, UserManager<User> userManager) {
+            _userManager = userManager;
             _context = context;
         }
 
@@ -18,18 +23,38 @@ namespace DatingApp.API.Controllers {
         [HttpGet ("usersWithRoles")]
         public async Task<IActionResult> GetUsersWithRoles () {
             var userList = await _context.Users
-                .OrderBy(x => x.UserName)
-                .Select( user => new {
+                .OrderBy (x => x.UserName)
+                .Select (user => new {
                     Id = user.Id,
-                    UserName = user.UserName,
-                    Roles = (from userRole in user.UserRoles
-                                join role in _context.Roles
-                                on userRole.RoleId
-                                equals role.Id
-                                select role.Name).ToList()
-                }).ToListAsync();
+                        UserName = user.UserName,
+                        Roles = (from userRole in user.UserRoles join role in _context.Roles on userRole.RoleId equals role.Id select role.Name).ToList ()
+                }).ToListAsync ();
 
-            return Ok(userList);
+            return Ok (userList);
+        }
+        
+        [Authorize (Policy = "RequireAdminRole")]
+        [HttpPost ("editRoles/{userName}")]
+        public async Task<IActionResult> EditRoles (string userName, RoleEditDto roleEditDto) {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var selectedRoles = roleEditDto.RoleNames;
+
+            // selectedRoles = selectedRoles != null ? selectedRoes : new string[] {};
+            selectedRoles = selectedRoles ?? new string[] {};
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if(!result.Succeeded)
+                return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if(!result.Succeeded)
+                return BadRequest("Failed to remove the roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
         }
 
         [Authorize (Policy = "ModeratePhotoRole")]
